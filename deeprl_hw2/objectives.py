2ba2d0c9df14b741f05bf9f572e7c1413b5f7ebf
TerryCodeHard <terryliu.engsci@gmail.com>
2019-05-18 12:31:23 +0800

    Add files via upload

--- deeprl_hw2/objectives.py
+++ deeprl_hw2/objectives.py
@@ -2,7 +2,10 @@
 
 import tensorflow as tf
 import semver
+import numpy as np
+import keras.backend as K
 
+'''
 
 def huber_loss(y_true, y_pred, max_grad=1.):
     """Calculate the huber loss.
@@ -25,6 +28,37 @@ def huber_loss(y_true, y_pred, max_grad=1.):
       The huber loss.
     """
     return tf.losses.huber_loss(y_true, y_pred, weights=1.0, delta=max_grad, scope=None, loss_collection=tf.GraphKeys.LOSSES, reduction=Reduction.SUM_BY_NONZERO_WEIGHTS)
+'''
+def huber_loss(y_true, y_pred, max_grad=1.):
+    """Calculate the huber loss.
+    See https://en.wikipedia.org/wiki/Huber_loss
+    Parameters
+    ----------
+    y_true: np.array, tf.Tensor
+      Target value.
+    y_pred: np.array, tf.Tensor
+      Predicted value.
+    max_grad: float, optional
+      Positive floating point value. Represents the maximum possible
+      gradient magnitude.
+    Returns
+    -------
+    tf.Tensor
+      The huber loss.
+    """
+    assert max_grad > 0., 'The maximum gradient should be larger than 0 while calculating huber loss'
+
+    diff = y_true - y_pred
+    if np.isinf(max_grad):  # handle edge case
+        return .5 * K.square(diff)
+
+    condition = K.abs(diff) < max_grad
+    squared_loss = .5 * K.square(diff)
+    linear_loss = max_grad * (K.abs(diff) - .5 * max_grad)
+    if hasattr(tf, 'select'):
+        return tf.select(condition, squared_loss, linear_loss)  # if difference less than max_grad, linear loss
+    else:
+        return tf.where(condition, squared_loss, linear_loss)  # if not, square loss
 
 
 def mean_huber_loss(y_true, y_pred, max_grad=1.):
@@ -48,6 +82,5 @@ def mean_huber_loss(y_true, y_pred, max_grad=1.):
     tf.Tensor
       The mean huber loss.
     """
-    temp = tf.losses.huber_loss(y_true, y_pred, weights=1.0, delta=max_grad, scope=None,
-                                loss_collection=tf.GraphKeys.LOSSES, reduction=Reduction.SUM_BY_NONZERO_WEIGHTS)
+    temp = huber_loss(y_true, y_pred, weights=1.0, max_grad=1.)
     return tf.reduce_mean(temp)
